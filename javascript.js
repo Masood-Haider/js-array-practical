596a730e60700e56857e45580be88cfef55e6f3b
Masood-Haider <masood.haider.bangash1@gmail.com>
2024-06-27 12:18:52 +0500

    added

---
+++ javascript.js
@@ -0,0 +1,51 @@
+//assignment number 1
+// let ar=[1,2,3,4,5];
+// ar.shift();
+// ar.shift();
+
+// ar.push(6,7,8);
+// console.log(ar);
+
+//assignment number 2
+// let ar=[1,2,3,4,5];
+// let length=ar.length;
+// let  ind=[];
+
+// for(i=0;i<length;i++){
+//     if(ar[i]%2==0){
+//         let index=ar.indexOf(ar[i]);
+//             ind.push(index);
+        
+//     }
+
+// }
+// console.log(ind);
+
+ //assignment number 3
+// let ar=[3,4,5];
+// ar.unshift(1,2);
+// ar.pop();
+// let length=ar.length;
+// for(i=0;i<length;i++){
+//     console.log(ar[i]);
+// }
+
+// assignment number 4
+// let ar=[1,2,3,4,5];
+// let length=ar.length;
+
+// for(i=0;i<length;i++){
+//     if(ar[i]>ar[i-1]){
+//         console.log(ar[i]);
+//     }
+// }
+
+// assignment number 5
+// let ar=[1,2,3,4,5];
+// let ind=ar.indexOf(3);
+// length=ar.length;
+// ar.splice(ind,length,);
+
+//  for(i=0;i<length;i++){
+//     console.log(ar[i]);
+//  }
